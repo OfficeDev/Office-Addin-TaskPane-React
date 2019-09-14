12d18c1820220062054cbcd926f3590266b04891
Courtney Owen <cowen@microsoft.com>
2019-09-14 08:38:12 -0700

    Merge office-addin-taskpane/master
    NOTE: Ran lint:fix and addressed issues, however there are still unused-vars errors that appears to be a known issue with linting and React (e.g. https://stackoverflow.com/questions/42541559/eslint-with-react-gives-no-unused-vars-errors)

--- src/taskpane/components/Excel.App.tsx
+++ src/taskpane/components/Excel.App.tsx
@@ -1,8 +1,9 @@
-import * as React from 'react';
-import { Button, ButtonType } from 'office-ui-fabric-react';
-import Header from './Header';
-import HeroList, { HeroListItem } from './HeroList';
-import Progress from './Progress';
+import * as React from "react";
+import { Button, ButtonType } from "office-ui-fabric-react";
+import Header from "./Header";
+import HeroList, { HeroListItem } from "./HeroList";
+import Progress from "./Progress";
+/* global Button, console, Excel, Header, HeroList, HeroListItem, Progress */
 
 export interface AppProps {
   title: string;
@@ -25,16 +26,16 @@ export default class App extends React.Component<AppProps, AppState> {
     this.setState({
       listItems: [
         {
-          icon: 'Ribbon',
-          primaryText: 'Achieve more with Office integration'
+          icon: "Ribbon",
+          primaryText: "Achieve more with Office integration"
         },
         {
-          icon: 'Unlock',
-          primaryText: 'Unlock features and functionality'
+          icon: "Unlock",
+          primaryText: "Unlock features and functionality"
         },
         {
-          icon: 'Design',
-          primaryText: 'Create and visualize like a pro'
+          icon: "Design",
+          primaryText: "Create and visualize like a pro"
         }
       ]
     });
@@ -60,30 +61,32 @@ export default class App extends React.Component<AppProps, AppState> {
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   render() {
-    const {
-      title,
-      isOfficeInitialized,
-    } = this.props;
+    const { title, isOfficeInitialized } = this.props;
 
     if (!isOfficeInitialized) {
       return (
-        <Progress
-          title={title}
-          logo='assets/logo-filled.png'
-          message='Please sideload your addin to see app body.'
-        />
+        <Progress title={title} logo="assets/logo-filled.png" message="Please sideload your addin to see app body." />
       );
     }
 
     return (
-      <div className='ms-welcome'>
-        <Header logo='assets/logo-filled.png' title={this.props.title} message='Welcome' />
-        <HeroList message='Discover what Office Add-ins can do for you today!' items={this.state.listItems}>
-          <p className='ms-font-l'>Modify the source files, then click <b>Run</b>.</p>
-          <Button className='ms-welcome__action' buttonType={ButtonType.hero} iconProps={{ iconName: 'ChevronRight' }} onClick={this.click}>Run</Button>
+      <div className="ms-welcome">
+        <Header logo="assets/logo-filled.png" title={this.props.title} message="Welcome" />
+        <HeroList message="Discover what Office Add-ins can do for you today!" items={this.state.listItems}>
+          <p className="ms-font-l">
+            Modify the source files, then click <b>Run</b>.
+          </p>
+          <Button
+            className="ms-welcome__action"
+            buttonType={ButtonType.hero}
+            iconProps={{ iconName: "ChevronRight" }}
+            onClick={this.click}
+          >
+            Run
+          </Button>
         </HeroList>
       </div>
     );
