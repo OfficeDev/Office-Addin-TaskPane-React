b2748583897cefc7fec67e067b7343765cf788e3
Courtney Owen <cowen@microsoft.com>
2019-10-08 15:27:15 -0700

    Merge office-addin-taskpane/master

--- src/taskpane/components/Project.App.tsx
+++ src/taskpane/components/Project.App.tsx
@@ -3,7 +3,7 @@ import { Button, ButtonType } from "office-ui-fabric-react";
 import Header from "./Header";
 import HeroList, { HeroListItem } from "./HeroList";
 import Progress from "./Progress";
-/* global Button, Header, HeroList, HeroListItem, Progress */
+/* global Button, console, document, Header, HeroList, HeroListItem, Office, Progress */
 
 export interface AppProps {
   title: string;
@@ -42,9 +42,34 @@ export default class App extends React.Component<AppProps, AppState> {
   }
 
   click = async () => {
-    /**
-     * Insert your Project code here
-     */
+    try {
+      // Get the GUID of the selected task
+      Office.context.document.getSelectedTaskAsync(result => {
+        let taskGuid;
+        if (result.status === Office.AsyncResultStatus.Succeeded) {
+          taskGuid = result.value;
+
+          // Set the specified fields for the selected task.
+          const targetFields = [Office.ProjectTaskFields.Name, Office.ProjectTaskFields.Notes];
+          const fieldValues = ["New task name", "Notes for the task."];
+
+          // Set the field value. If the call is successful, set the next field.
+          for (let index = 0; index < targetFields.length; index++) {
+            Office.context.document.setTaskFieldAsync(taskGuid, targetFields[index], fieldValues[index], result => {
+              if (result.status === Office.AsyncResultStatus.Succeeded) {
+                index++;
+              } else {
+                console.log(result.error);
+              }
+            });
+          }
+        } else {
+          console.log(result.error);
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   render() {
