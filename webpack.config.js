47ce5ca61dac7cfb915ebc376237ee7c2586154c
Bruno Carvalhal <private>
2023-04-27 12:40:18 +0200

    Make sure polyfills are loaded first

--- webpack.config.js
+++ webpack.config.js
@@ -83,7 +83,8 @@ module.exports = async (env, options) => {
       new HtmlWebpackPlugin({
         filename: "taskpane.html",
         template: "./src/taskpane/taskpane.html",
-        chunks: ["taskpane", "vendor", "polyfills"],
+        chunksSortMode: "manual",
+        chunks: ["polyfill", "vendor", "taskpane"],
       }),
       new HtmlWebpackPlugin({
         filename: "commands.html",
