21c69930b98b0d3edfcc9a73a1e219b4f94170b0
Darren Miller <darrmill@microsoft.com>
2023-04-27 17:20:26 -0700

    Correct the webpack polyfill entry reference

--- src/taskpane/components/HeroList.tsx
+++ src/taskpane/components/HeroList.tsx
@@ -22,11 +22,11 @@ export default class HeroList extends React.Component<HeroListProps> {
       </li>
     ));
     return (
-      <main className="ms-welcome__main">
+      <div className="ms-welcome__main">
         <h2 className="ms-font-xl ms-fontWeight-semilight ms-fontColor-neutralPrimary ms-u-slideUpIn20">{message}</h2>
         <ul className="ms-List ms-welcome__features ms-u-slideUpIn10">{listItems}</ul>
         {children}
-      </main>
+      </div>
     );
   }
 }
