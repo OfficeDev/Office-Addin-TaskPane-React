417f2dd129e87c194f466ae4cccda08089dedf33
Darren Miller <darrmill@microsoft.com>
2024-02-08 10:35:52 -0800

    throw powerpoint error

--- src/taskpane/powerpoint-office-document.ts
+++ src/taskpane/powerpoint-office-document.ts
@@ -7,7 +7,7 @@ const insertText = async (text: string) => {
     { coercionType: Office.CoercionType.Text },
     (asyncResult: Office.AsyncResult<void>) => {
       if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-        Promise.reject(asyncResult.error);
+        throw asyncResult.error;
       }
     }
   );
