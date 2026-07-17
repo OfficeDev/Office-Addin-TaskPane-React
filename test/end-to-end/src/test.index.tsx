2b06e8de2ee039555e03df2d0f79fa76f3c391af
Darren Miller <darrmill@microsoft.com>
2026-07-17 13:25:47 -0700

    Update tests to handle more failures.

--- test/end-to-end/src/test.index.tsx
+++ test/end-to-end/src/test.index.tsx
@@ -2,8 +2,9 @@ import * as React from "react";
 import { createRoot } from "react-dom/client";
 import App from "../../../src/taskpane/components/App";
 import { FluentProvider, webLightTheme } from "@fluentui/react-components";
-import { pingTestServer } from "office-addin-test-helpers";
+import { pingTestServer, sendTestResults } from "office-addin-test-helpers";
 import { testExcelEnd2End, testPowerPointEnd2End, testWordEnd2End } from "./host-tests";
+import * as testHelpers from "./test-helpers";
 
 /* global document, Office, module, require */
 
@@ -16,26 +17,35 @@ const root = createRoot(rootElement);
 
 /* Render application after Office initializes */
 Office.onReady(async (info) => {
-  const testServerResponse: { status?: number } = await pingTestServer(port);
-  if (testServerResponse?.status === 200) {
-    //render(App);
-    root.render(
-      <FluentProvider theme={webLightTheme}>
-        <App title={title} />
-      </FluentProvider>
-    );
+  let testValues: any = [];
+  try {
+    const testServerResponse: { status?: number } = await pingTestServer(port);
+    if (testServerResponse?.status === 200) {
+      //render(App);
+      root.render(
+        <FluentProvider theme={webLightTheme}>
+          <App title={title} />
+        </FluentProvider>
+      );
 
-    switch (info.host) {
-      case Office.HostType.Excel: {
-        return testExcelEnd2End(port);
+      switch (info.host) {
+        case Office.HostType.Excel: {
+          return testExcelEnd2End(port);
+        }
+        case Office.HostType.PowerPoint: {
+          return testPowerPointEnd2End(port);
+        }
+        case Office.HostType.Word: {
+          return testWordEnd2End(port);
+        }
       }
-      case Office.HostType.PowerPoint: {
-        return testPowerPointEnd2End(port);
-      }
-      case Office.HostType.Word: {
-        return testWordEnd2End(port);
-      }
+    } else {
+      testHelpers.addErrorResult(testValues, `Ping failed: ${JSON.stringify(testServerResponse)}`);
+      await sendTestResults(testValues, port).catch(() => {});
     }
+  } catch (err) {
+    testHelpers.addErrorResult(testValues, `Initialization failed: ${testHelpers.formatError(err)}`);
+    await sendTestResults(testValues, port).catch(() => {});
   }
 });
 
